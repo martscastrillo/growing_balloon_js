06b939cb3416e9cecf356a34f47fe5893512c4f4
martscastrillo <mart.castrillo@gmail.com>
2023-03-11 18:20:59 +0100

    add new colors

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 "use strict";
 
-const ballon=document.querySelector(".js_balloon");
+ const balloon=document.querySelector(".js_balloon");
 
-function changecolor() {
-    console.log('helou')
-}
+ const intro = document.getElementById('intro');
+ let widthBalloon = 200;
+  let heightBalloon =200;
 
-ballon.addEventListener("click", changecolor);
+  function changecolor() {
+  let color ='#FF0000';
+  intro.style.width = `${widthBalloon+40}px`;
+  intro.style.height = `${heightBalloon+40}px`;
+    if(widthBalloon === 420 && heightBalloon===420){
+       widthBalloon = 200;
+       heightBalloon = 200;
+    }else if(widthBalloon >=200 && heightBalloon>=200){
+       widthBalloon = widthBalloon +10;
+       heightBalloon = heightBalloon +10;
+    }
 
+
+   if(balloon.classList.contains("colorRed")){
+       balloon.classList.remove("colorRed");
+       balloon.classList.add("colorGreen")
+     }else if(balloon.classList.contains("colorGreen")){
+       balloon.classList.remove("colorGreen");
+       balloon.classList.add("colorYellow")
+     }else if(balloon.classList.contains("colorYellow")){
+       balloon.classList.remove("colorYellow");
+       balloon.classList.add("colorRed")
+      }
+
+ }
+
+ balloon.addEventListener("click", changecolor);
+ balloon.addEventListener("mouse", changecolor);
